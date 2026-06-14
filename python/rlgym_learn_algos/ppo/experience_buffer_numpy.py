33362a5219fe35ee87277d2cd13e1762c34b1ccf
Jonathan Keegan <jpk3.14@gmail.com>
2026-06-14 17:54:01 -0400

    clean up experience_buffer_numpy.py

--- python/rlgym_learn_algos/ppo/experience_buffer_numpy.py
+++ python/rlgym_learn_algos/ppo/experience_buffer_numpy.py
@@ -1,9 +1,16 @@
+# pyright: reportUnknownMemberType=false, reportIncompatibleVariableOverride=false, reportIncompatibleMethodOverride=false, reportMissingSuperCall=false
+
+from collections.abc import Generator, Sequence
+from typing import Any, cast
+
 import numpy as np
 import torch
+from numpy.typing import NDArray
 from rlgym.api import AgentID, RewardType
 from typing_extensions import override
 
 from .experience_buffer import ExperienceBuffer
+from .trajectory import Trajectory
 from .trajectory_processor import (
     TrajectoryProcessor,
     TrajectoryProcessorConfig,
@@ -11,7 +18,6 @@ from .trajectory_processor import (
 )
 
 
-# Ignore this for now, experience buffers will get refactored to use tensordicts and then this won't be needed
 class NumpyExperienceBuffer(
     ExperienceBuffer[
         TrajectoryProcessorConfig,
@@ -24,11 +30,10 @@ class NumpyExperienceBuffer(
 ):
     @staticmethod
     def _cat_numpy(t1: np.ndarray | None, t2: np.ndarray, size: int):
-        t2 = np.array(t2)
         t2_len = len(t2)
         if t1 is None:
             if t2_len > size:
-                t = t2[-size:].copy()
+                t = cast(np.ndarray, t2[-size:].copy())
             else:
                 t = t2
             del t1
@@ -37,8 +42,8 @@ class NumpyExperienceBuffer(
 
         if t2_len > size:
             # t2 alone is larger than we want; copy the end
-            # This clone is needed to avoid nesting views
-            t = t2[-size:].copy()
+            # This copy is needed to avoid nesting views
+            t = cast(np.ndarray, t2[-size:].copy())
 
         elif t2_len == size:
             # t2 is a perfect match; just use it directly
@@ -83,7 +88,10 @@ class NumpyExperienceBuffer(
         self.advantages: torch.Tensor = torch.FloatTensor()
 
     @override
-    def submit_experience(self, trajectories):
+    def submit_experience(
+        self,
+        trajectories: list[Trajectory[AgentID, np.ndarray, np.ndarray, RewardType]],
+    ) -> TrajectoryProcessorData:
         _cat = ExperienceBuffer._cat
         _cat_list = ExperienceBuffer._cat_list
         _cat_numpy = NumpyExperienceBuffer._cat_numpy
@@ -99,11 +107,13 @@ class NumpyExperienceBuffer(
         )
         self.observations = _cat_numpy(
             self.observations,
-            observations,
+            np.array(observations),
             self.config.experience_buffer_config.max_size,
         )
         self.actions = _cat_numpy(
-            self.actions, actions, self.config.experience_buffer_config.max_size
+            self.actions,
+            np.array(actions),
+            self.config.experience_buffer_config.max_size,
         )
         self.log_probs = _cat(
             self.log_probs,
@@ -123,9 +133,23 @@ class NumpyExperienceBuffer(
 
         return trajectory_processor_data
 
-    def _get_samples(self, indices):
+    @override
+    def _get_samples(
+        self, indices: NDArray[np.long]
+    ) -> tuple[
+        Sequence[AgentID],
+        np.ndarray,
+        np.ndarray,
+        torch.Tensor,
+        torch.Tensor,
+        torch.Tensor,
+    ]:
+        assert self.observations is not None and self.actions is not None, (
+            "Can't get samples before any data has been added to the experience buffer!"
+        )
+        py_indices: list[int] = indices.tolist()
         return (
-            [self.agent_ids[index] for index in indices],
+            [self.agent_ids[index] for index in py_indices],
             self.observations[indices],
             self.actions[indices],
             self.log_probs[indices],
@@ -134,7 +158,20 @@ class NumpyExperienceBuffer(
         )
 
     @override
-    def get_all_batches_shuffled(self, batch_size):
+    def get_all_batches_shuffled(
+        self, batch_size: int
+    ) -> Generator[
+        tuple[
+            Sequence[AgentID],
+            np.ndarray,
+            np.ndarray,
+            torch.Tensor,
+            torch.Tensor,
+            torch.Tensor,
+        ],
+        Any,
+        None,
+    ]:
         """
         Function to return the experience buffer in shuffled batches. Code taken from the stable-baeselines3 buffer:
         https://github.com/DLR-RM/stable-baselines3/blob/2ddf015cd9840a2a1675f5208be6eb2e86e4d045/stable_baselines3/common/buffers.py#L482
@@ -150,6 +187,7 @@ class NumpyExperienceBuffer(
             yield self._get_samples(indices[start_idx : start_idx + batch_size])
             start_idx += batch_size
 
+    @override
     def clear(self):
         """
         Function to clear the experience buffer.
@@ -160,4 +198,9 @@ class NumpyExperienceBuffer(
         del self.log_probs
         del self.values
         del self.advantages
-        self.__init__(self.max_size, self.seed, self.device)
+        self.agent_ids = []
+        self.observations = None
+        self.actions = None
+        self.log_probs = torch.FloatTensor()
+        self.values = torch.FloatTensor()
+        self.advantages = torch.FloatTensor()
